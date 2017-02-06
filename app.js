43b9bf10ea97b1f3599280383abcd69f7bcf9483
justinmcnary <justinmcnary@yahoo.com>
2017-02-06 10:25:58 -0800

    updated answer system

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
+var quizHtml = 
+`<p class="question"></p>
+  <ul class="s">
+  <input type="radio" name=" " value=" "><br/>
+  <input type="radio" name=" " value=" "><br/>
+  <input type="radio" name=" " value=" "><br/>
+  <input type="radio" name=" " value=" "><br/>`
+
 var state = {
-  questions: [
-    {title: "Question Text",
-     answers: [
-       {letter: "a", text: "Blue", value: "2" },
-       {letter: "b", text: "Green", value: "4" },
-       {letter: "c", text: "Red", value: "6" },
-       {letter: "d", text: "Orange", value: "8" },
-     ]
+  questions:[
+    {
+   question:"What is the name of the species that compelled Capt. James T. Kirk and Lt. Nyota Uhura to kiss?", 
+   choices:["The Platonians", "The Vulcans", "The Romulans", "The Kazon"],
+   correct: 1
     },
   ],
-  score: {correct: 0, incorrect: 0},
-
-};
-function getClickedAnswerValue (state, item) {
-
 }
+
 // modify state functions
 
 //render functions
-function displayNextQuestion (state, question_number) {
-  $('.startpage').addClass('hidden');
-  $('.quiz').removeClass('hidden');
-  console.log(state);
+function displayNextQuestion (state, question_number, element) {
+  // $('.startpage').addClass('hidden');
+  var currentquestion = state.questions[0].question; 
+  console.log(currentquestion);
+  
+  // $('.quiz').removeClass('hidden');
+  // console.log(state);
 }
 
 //event handlers
 $('.begin').on('click', function(event){
-  displayNextQuestion();
+  displayNextQuestion(state, 0, $('.question'));
 });

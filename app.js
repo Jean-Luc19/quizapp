5fe4cbba587f1982f3756510f80b548c5da81a63
Jean-Luc19 <aticus19@gmail.com>
2017-02-05 17:09:14 -0700

    linked to style sheet, jQuery and apps.js from index.html, changed html layout

---
+++ app.js
@@ -0,0 +1,25 @@
+var state = {
+  questions: [
+    {title: "Question Text",
+     answers: [
+       {letter: "a", text: "Blue", value: "2" },
+       {letter: "b", text: "Green", value: "4" },
+       {letter: "c", text: "Red", value: "6" },
+       {letter: "d", text: "Orange", value: "8" },
+     ]
+    },
+    {title: "Question Text",
+     answers: [
+       {letter: "a", text: "Blue", value: "2" },
+       {letter: "b", text: "Green", value: "4" },
+       {letter: "c", text: "Red", value: "6" },
+       {letter: "d", text: "Orange", value: "8" },
+     ]
+    },
+  ],
+  score: {correct: 0, incorrect: 0},
+
+};
+function getClickedAnswerValue (state, item) {
+
+};

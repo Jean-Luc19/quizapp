368dc3d9f8961340de3177fdb2d7c3c5a9a7f38c
Jean-Luc19 <aticus19@gmail.com>
2017-02-06 12:35:37 -0700

    got render function working

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
-var quizHtml = 
+var quizHtml =
 `<p class="question"></p>
-  <ul class="s">
-  <input type="radio" name=" " value=" "><br/>
-  <input type="radio" name=" " value=" "><br/>
-  <input type="radio" name=" " value=" "><br/>
-  <input type="radio" name=" " value=" "><br/>`
+  <ul class="s">`
+
 
 var state = {
   questions:[
     {
-   question:"What is the name of the species that compelled Capt. James T. Kirk and Lt. Nyota Uhura to kiss?", 
+   question:"What is the name of the species that compelled Capt. James T. Kirk and Lt. Nyota Uhura to kiss?",
    choices:["The Platonians", "The Vulcans", "The Romulans", "The Kazon"],
-   correct: 1
+   correct: 0;
     },
   ],
-}
+  //score
+  //currentQuestion index
+};
 
 // modify state functions
 
 //render functions
 function displayNextQuestion (state, question_number, element) {
   // $('.startpage').addClass('hidden');
-  var currentquestion = state.questions[0].question; 
-  console.log(currentquestion);
-  
+  $('.startpage').addClass('hidden');
+  var currentquestion = state.questions[0];
+  var $display = $('.quiz');
+  $display.removeClass('hidden');
+  var questionHTML = `<p class="question">${currentquestion.question}</p>`;
+  currentquestion.choices.forEach(function (answer, i) {
+    questionHTML += `<button id='${i}'> ${answer} </button>`;
+  });
+  $display.html(questionHTML);
+  console.log(questionHTML);
+
+
   // $('.quiz').removeClass('hidden');
   // console.log(state);
 }
